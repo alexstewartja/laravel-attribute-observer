94c736318f4017d045ac9f32a89f749d7f2efdad
A. Stewart <iamalexstewart@gmail.com>
2021-12-02 10:18:21 -0500

    Added tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,3 +3,13 @@
 use AlexStewartJA\LaravelAttributeObserver\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+
+test('generates an attribute observer', function () {
+    $this->artisan('make:laravel-attribute-observer TestAttributeObserver')
+        ->assertExitCode(0);
+});
+
+test('generates an attribute observer (for an Eloquent Model)', function () {
+    $this->artisan('make:laravel-attribute-observer TestUserAttributeObserver --model=User')
+        ->assertExitCode(0);
+});
